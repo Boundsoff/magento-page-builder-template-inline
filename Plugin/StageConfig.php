6ee62fb3ab3f629ab4059f6d413157022840bd26
Kamil Wiśniowski <kwisniowski@boundsoff.com>
2025-01-01 15:30:03 +0100

    add sample unit tests

--- Plugin/StageConfig.php
+++ Plugin/StageConfig.php
@@ -8,12 +8,12 @@ use Magento\Framework\UrlInterface;
 
 class StageConfig
 {
+    const ROUTE_PATH = 'Boundsoff_PageBuilderTemplateInline/template/save';
 
     public function __construct(
         protected readonly UrlInterface $urlBuilder,
         protected readonly AuthorizationInterface $authorization,
-    )
-    {
+    ) {
     }
 
     /**
@@ -24,7 +24,7 @@ class StageConfig
      */
     public function afterGetConfig($subject, $result)
     {
-        $result['bf__template_save_url'] = $this->urlBuilder->getUrl('Boundsoff_PageBuilderTemplateInline/template/save');
+        $result['bf__template_save_url'] = $this->urlBuilder->getUrl(static::ROUTE_PATH);
 
         foreach (ConfigAcl::cases() as $case) {
             $result['acl'][strtolower($case->name)] = $this->authorization->isAllowed($case->value);
